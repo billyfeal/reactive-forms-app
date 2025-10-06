ef3d3891e18ef1208ad558470768536e27418359
William Feal <william.feal@emburse.com>
2025-10-06 15:38:56 -0400

    Adding validations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ArgumentOutOfRangeError } from 'rxjs';
 
 @Component({
@@ -12,10 +12,10 @@ export class BasicPageComponent {
 
   private fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
-    name: [''],
-    price: [0],
-    amount: [0]
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0.1)]],
+    amount: [0, [Validators.required, Validators.min(0)]]
   });
 
   // myForm = new FormGroup({
@@ -23,5 +23,28 @@ export class BasicPageComponent {
   //   price: new FormControl(0),
   //   amount: new FormControl(0)
   // });
+
+  isValidField(fieldName: string): boolean | null {
+    return !! this.myForm.controls[fieldName].errors;
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `Min ${errors['minlength'].requiredLength} characters`;
+        case 'min':
+          return `Min value is ${errors['min'].min}`;
+      }
+    }
+
+    return null;
+  }
+
 
  }
